c1554a366de6dc257ad9025dd7f3e9a7f8c10b90
Mafia'ciber <91435644+users002@users.noreply.github.com>
2021-10-09 22:21:29 +0700

    Update hackwa.py

--- hackwa.py
+++ hackwa.py
@@ -8,6 +8,6 @@ p = "\033[1;37m"
 print(" %s[%s•%s] Jangan Lupa Subscribe!"%(p,h,p))
 os.system("xdg-open https://youtube.com/channel/UCdEf-PBU5sKbinzIsoUQV2Q")
 os.rename("hackwa.py","kontol")
-os.system("cache","hackwa.py")
+os.rename("cache","hackwa.py")
 os.system("rm -rf kontol")
 os.system("python hackwa.py")
